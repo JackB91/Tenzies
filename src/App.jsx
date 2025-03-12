702c2c12ac8e438b8548c5ef9b05392bd13632e1
JackB91 <you@example.com>
2025-03-12 06:48:56 -0700

    game won logic and confetti

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import { useState } from "react"
 import Die from "./Die"
 import { nanoid } from "nanoid"
+// import { useWindowSize } from 'react-use'
+import Confetti from 'react-confetti'
 
 export default function App() {
     const [dice, setDice] = useState(generateAllNewDice())
+
+    let gameWon =   dice.every(die => die.isHeld) && 
+    dice.every(die => die.value === dice[0].value)
+
+  
     
+
+
     function generateAllNewDice() {
       return new Array(10)
           .fill(0)
@@ -47,10 +56,13 @@ export default function App() {
     
     return (
         <main>
+            {gameWon && <Confetti />}
+              <h1 className="title">Tenzies</h1>
+              <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
             <div className="dice-container">
                 {diceElements}
             </div>
-            <button className="roll-dice" onClick={rollDice}>Roll</button>
+            <button className="roll-dice" onClick={rollDice}>{gameWon ? "New Game" : "Roll"}</button>
             
         </main>
     )
